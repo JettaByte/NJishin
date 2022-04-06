01d853cc38c006ca1710a59501e808fb7e16839a
ビュインプン <ingpungya@gmail.com>
2022-04-06 17:12:25 +0900

    UI5

--- js/eew.js
+++ js/eew.js
@@ -1,12 +1,64 @@
-// const request = new XMLHttpRequest();
+const request = new XMLHttpRequest();
 
-// setInterval(() => {
-//     var url = 'http://www.kmoni.bosai.go.jp/webservice/hypo/eew/20220316233830.json'; // 긁어오고 싶은 주소를 넣는다. 예제는 네이버
+setInterval(() => {
+    var j = document.createElement('script');
+    var url = 'http://www.kmoni.bosai.go.jp/webservice/hypo/eew/20220316233830.json';
+  
+    j.setAttribute('src', url);
+    j.setAttribute('type', 'text/javascript');
+    var re = document.getElementsByTagName('body')[0].appendChild(j);
+    console.log(re)
+    return false;
+  }, 1000);
 
-//     request.open('GET', url, true);
-//     request.onload = function () {
-//         console.log(request.responseText); // 긁어온 내용 뿌리기
-//     };
-//     request.send();
-//     console.log('http://www.kmoni.bosai.go.jp/webservice/hypo/eew에서 현재 긴급지진속보 발령 정보를 불러왔습니다.');
-//   }, 1000);
+//반복문 예전코드
+    // var url = 'http://www.kmoni.bosai.go.jp/webservice/hypo/eew/20220316233830.json';
+    // request.open('GET', url, true);
+    // request.onload = function () {
+    //     console.log(request.responseText); // 임시
+    // };
+    // request.send();
+
+// 예제 코드
+//   function find()
+//   {
+//       var j = document.createElement('script');
+//       var url = 'http://www.kmoni.bosai.go.jp/webservice/hypo/eew/20220316233830.json';
+    
+//       j.setAttribute('src', u);
+//       j.setAttribute('type', 'text/javascript');
+//       var re = document.getElementsByTagName('pre').appendChild(j);
+//       console.log(re)
+//       return false;
+//   }
+
+// function find()
+// {
+//     var j = document.createElement('script');
+//     var s = encodeURIComponent(document.getElementById('str').value);
+//     var u = 'http://b.epiloum.net/find.php?callback=loadList&s=' + s;
+  
+//     j.setAttribute('src', u);
+//     j.setAttribute('type', 'text/javascript');
+//     document.getElementsByTagName('body')[0].appendChild(j);
+ 
+//     return false;
+// }
+ 
+// function loadList(arr)
+// {
+//     var o = document.getElementById('list');
+//     var l;
+  
+//     while(o.childNodes.length)
+//     {
+//         o.removeChild(o.lastChild);
+//     }
+  
+//     for(var i=0; i<arr.length; i++)
+//     {
+//         l = document.createElement('li');
+//         l.appendChild(document.createTextNode(arr[i]));
+//         o.appendChild(l);
+//     }
+// }
