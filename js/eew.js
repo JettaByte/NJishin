2bd82f8cf7bd8bf79482734f246e3c6befe55ba4
ビュインプン <ingpungya@gmail.com>
2022-04-06 13:12:33 +0900

    UI4

---
+++ js/eew.js
@@ -0,0 +1,12 @@
+// const request = new XMLHttpRequest();
+
+// setInterval(() => {
+//     var url = 'http://www.kmoni.bosai.go.jp/webservice/hypo/eew/20220316233830.json'; // 긁어오고 싶은 주소를 넣는다. 예제는 네이버
+
+//     request.open('GET', url, true);
+//     request.onload = function () {
+//         console.log(request.responseText); // 긁어온 내용 뿌리기
+//     };
+//     request.send();
+//     console.log('http://www.kmoni.bosai.go.jp/webservice/hypo/eew에서 현재 긴급지진속보 발령 정보를 불러왔습니다.');
+//   }, 1000);
